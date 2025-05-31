d9d86fc3dc435dc2316362f30e5f03d6f0fbe912
Matt443 <marcin26050606@gmail.com>
2025-05-31 15:01:30 +0200

    Added meta tags

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 const state = useStateStore();
 state.changeLanguage("en");
+
+useSeoMeta({
+    title: "Ip Calculator",
+    ogTitle: "Ip Calculator",
+    description:
+        "An IP calculator for computing the network address, broadcast address, number of hosts, and subnets, including support for the VLSM method.",
+    ogDescription:
+        "An IP calculator for computing the network address, broadcast address, number of hosts, and subnets, including support for the VLSM method.",
+    ogImage: "https://ip-calculator.marcinregula.de/icon.png",
+    twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
